17a1f6075b155e658657ffb259fbe91f5c570a4e
Raojun <comeon.focus@gmail.com>
2022-06-21 11:34:36 +0800

    feat

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import './index.css'
 // import xiao from './assets/xiao.jpeg';
 const a = () => {
-  console.log(12);
+  console.log(12)
 }
-const b = [1,1,2,3,4,8,34,10000];
+const b = [1,1,2,3,4,8,34,10000]
 a()
-console.log(b.includes(400));
-var c = 1;
+console.log(b.includes(400))
+const c = 1
